13330e5f666ef4dc038d54d01736960fd3501118
Alex Tsabii <lsashokel@gmail.com>
2023-09-14 16:55:07 +0300

    homeWork_7

---
+++ src/index.js
@@ -0,0 +1,25 @@
+// 1) Створіть функцію reverseArray, яка приймає масив і повертає новий масив, елементи якого розташовані у зворотному порядку.
+
+
+const originalArray = [1, 2, 3, 4, 5];
+const reversedArray = reverseArray(originalArray);
+
+function reverseArray(originalArray) {
+  return originalArray.reverse();
+}
+
+console.log(reversedArray);
+
+
+// 2) Створіть функцію uniqueValues, яка приймає два масиви і повертає новий масив, що містить тільки унікальні значення з обох масивів (без дублікатів).
+
+// const array1 = [1, 2, 3, 4, 5];
+// const array2 = [3, 4, 5, 6, 7];
+// const uniqueValuesArray = uniqueValues(array1, array2);
+
+// function uniqueValues (array1, array2) {
+//   return 
+// }
+
+
+// console.log(uniqueValuesArray);
