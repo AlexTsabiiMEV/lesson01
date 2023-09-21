3610780feef84899b65b582e3e58a5ed7fc1a785
Alex Tsabii <lsashokel@gmail.com>
2023-09-21 15:33:39 +0300

    Initial home work 9

--- src/index.js
+++ src/index.js
@@ -1,45 +1,59 @@
-// 1) Напишіть функцію capitalizeStrings, яка приймає на вхід масив рядків і повертає новий масив, у якому кожен рядок має першу літеру у верхньому регістрі, а решту літер - у нижньому регістрі.
+// 1) Вам необхідно написати функцію, яка приймає на вхід масив чисел і повертає новий масив, що містить тільки ті числа, які є простими числами.
+
+// const array = [1,2,3,4,5,6,7,8,9,10,100,33,32,49,72,1235,77,824,0,12489];
+// const primeNumbers = primesArray(array);
 
 
-// const words = ["apple", "banaNA", "kiWi", "ORANGE"];
+// function primesArray(array) {
+//   function isPrime(array) {
+//     if (array <= 1) return false;
+//     if (array <= 3) return true;
+//     if (array % 2 === 0 || array % 3 === 0) return false;
+//     for (let i = 5; i * i <= array; i += 6) {
+//       if (array % i === 0 || array % (i + 2) === 0) return false;
+//     }
+//     return true;
+//   }
 
-
-// function capitalizeStrings (words) { 
-
-//   let capitalizeWord = words.map(item => {
-//     const capitalizeFirstLetter = item.charAt(0).toUpperCase()
-//     const restItems = item.slice(1).toLowerCase();
-//     return capitalizeFirstLetter + restItems;
-//   })
-
-//   return capitalizeWord;
-
+//   return array.filter(isPrime);
 // }
 
-// console.log(capitalizeStrings(words))
+// console.log(primeNumbers); 
 
 
-// 2) Напишіть функцію findCommonElements, яка приймає на вхід два масиви і повертає новий масив, що містить елементи, які є в обох вихідних масивах.
+// 2) Вам необхідно написати функцію, яка приймає на вхід масив об'єктів, де кожен об'єкт описує сповіщення та має поля source / text / date. Вам необхідно перетворити цей масив на об'єкт, де ключем буде джерело сповіщення, а значенням - масив сповіщень із цього джерела.
 
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [3, 4, 5, 6, 7];
+const currentWeatherNotification = [
+  {source:'Cherkasy',text:'It is 21 degrees in Cherkasy today.',date:'2023-09-21 13:31:32'},
+  {source:'Cherkasy',text:'It is 22 degrees in Cherkasy today.',date:'2023-09-21 15:17:19'},
+  {source:'Cherkasy',text:'It is 23 degrees in Cherkasy today.',date:'2023-09-21 15:17:19'},
+  {source:'Cherkasy',text:'It is 21 degrees in Cherkasy today.',date:'2023-09-21 15:17:19'},
+  {source:'Kiyv',text:'It is 22 degrees in Kiyv today.',date:'2023-09-21 14:27:10'},
+  {source:'Kiyv',text:'It is 22 degrees in Kiyv today.',date:'2023-09-21 15:37:10'},
+  {source:'Kiyv',text:'It is 22 degrees in Kiyv today.',date:'2023-09-21 16:67:10'},
+  {source:'Kharkiv',text:'It is 24 degrees in Kharkiv today.',date:'2023-09-21 14:11:21'},
+  {source:'Kharkiv',text:'It is 24 degrees in Kharkiv today.',date:'2023-09-21 15:26:31'},
+  {source:'Kharkiv',text:'It is 24 degrees in Kharkiv today.',date:'2023-09-21 17:40:24'},
+  {source:'Kharkiv',text:'It is 24 degrees in Kharkiv today.',date:'2023-09-21 19:53:51'},
+  {source:'Dnipro',text:'It is 25 degrees in Dnipro today.',date:'2023-09-21 15:24:48'},
+  {source:'Lviv',text:'It is 20 degrees in Lviv today.',date:'2023-09-21 16:00:09'},
+  {source:'Lviv',text:'It is 20 degrees in Lviv today.',date:'2023-09-21 17:00:19'},
+];
 
-function findCommonElements (array1, array2) {
+const targetSource = 'Cherkasy';
 
-  const commonItems = [];
 
-  for (let i = 0; i < array1.length; i += 1) {
-
-    if (array2.includes(array1[i])){
-      commonItems.push(array1[i]);
-    }
+function weatherNotification (currentWeatherNotification) {
+for (var i = 0; i < currentWeatherNotification.length; i++) {
+  let notification = currentWeatherNotification[i];
+  
+  if (notification.source === targetSource) {
+    console.log(notification);
   }
-return commonItems;
-
+}
 }
 
-console.log(findCommonElements(array1, array2)); // [3, 4, 5]
-
+weatherNotification(currentWeatherNotification);
 
 
 
