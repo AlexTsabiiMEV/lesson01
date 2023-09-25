b059111fdb92ce743ef6b7c1bce5102612044ade
Alex Tsabii <lsashokel@gmail.com>
2023-09-25 18:14:34 +0300

    Home work 10

--- src/index.js
+++ src/index.js
@@ -1,58 +1,34 @@
-// 1) Вам необхідно написати функцію, яка приймає на вхід масив чисел і повертає новий масив, що містить тільки ті числа, які є простими числами.
+// 1) Вам необхідно використовувати масив нотифікацій з минулих занять. До отриманого під час групування об'єкта notifications, вам необхідно додати ітератор, щоб під час перебору в циклі for of ми отримували кожен елемент із вкладених списків об'єкта notifications таким чином, немов працюємо з "плоским" масивом.
 
-const array = [1,2,3,4,5,6,7,8,9,10,100,33,32,49,72,1235,77,824,0,12489];
-const primeNumbers = primesArray(array);
+const currentWeatherNotification = [
+  {source:'Cherkasy',text:'It is 21 degrees in Cherkasy today.',date:'2023-09-21 13:31:32'},
+  {source:'Cherkasy',text:'It is 22 degrees in Cherkasy today.',date:'2023-09-21 15:17:19'},
+  {source:'Cherkasy',text:'It is 23 degrees in Cherkasy today.',date:'2023-09-21 15:17:19'},
+  {source:'Cherkasy',text:'It is 21 degrees in Cherkasy today.',date:'2023-09-21 15:17:19'},
+  {source:'Kiyv',text:'It is 22 degrees in Kiyv today.',date:'2023-09-21 14:27:10'},
+  {source:'Kiyv',text:'It is 22 degrees in Kiyv today.',date:'2023-09-21 15:37:10'},
+  {source:'Kiyv',text:'It is 22 degrees in Kiyv today.',date:'2023-09-21 16:67:10'},
+  {source:'Kharkiv',text:'It is 24 degrees in Kharkiv today.',date:'2023-09-21 14:11:21'},
+  {source:'Kharkiv',text:'It is 24 degrees in Kharkiv today.',date:'2023-09-21 15:26:31'},
+  {source:'Kharkiv',text:'It is 24 degrees in Kharkiv today.',date:'2023-09-21 17:40:24'},
+  {source:'Kharkiv',text:'It is 24 degrees in Kharkiv today.',date:'2023-09-21 19:53:51'},
+  {source:'Dnipro',text:'It is 25 degrees in Dnipro today.',date:'2023-09-21 15:24:48'},
+  {source:'Lviv',text:'It is 20 degrees in Lviv today.',date:'2023-09-21 16:00:09'},
+  {source:'Lviv',text:'It is 20 degrees in Lviv today.',date:'2023-09-21 17:00:19'},
+];
 
+currentWeatherNotification[Symbol.iterator] = function() {
+  return {
+    notifications: this,
+    counter: 0,
 
-function isPrime(number) { 
-  if (number <= 1) return false;
-  if (number <= 3) return true;
-  if (number % 2 === 0 || number % 3 === 0) return false;
-  for (let i = 5; i * i <= number; i += 6) {
-    if (number % i === 0 || number % (i + 2) === 0) return false;
-  }
-  return true;
+    next() {
+      return { done: this.counter === this.notifications.length, value: this.notifications[this.counter++] }
+    }
+  };
 }
 
-function primesArray(array) {
-  return array.filter(isPrime);
+for (const notif of currentWeatherNotification) {
+  console.log(notif);
 }
-console.log(primeNumbers); 
-
-
-// 2) Вам необхідно написати функцію, яка приймає на вхід масив об'єктів, де кожен об'єкт описує сповіщення та має поля source / text / date. Вам необхідно перетворити цей масив на об'єкт, де ключем буде джерело сповіщення, а значенням - масив сповіщень із цього джерела.
-
-// const currentWeatherNotification = [
-//   {source:'Cherkasy',text:'It is 21 degrees in Cherkasy today.',date:'2023-09-21 13:31:32'},
-//   {source:'Cherkasy',text:'It is 22 degrees in Cherkasy today.',date:'2023-09-21 15:17:19'},
-//   {source:'Cherkasy',text:'It is 23 degrees in Cherkasy today.',date:'2023-09-21 15:17:19'},
-//   {source:'Cherkasy',text:'It is 21 degrees in Cherkasy today.',date:'2023-09-21 15:17:19'},
-//   {source:'Kiyv',text:'It is 22 degrees in Kiyv today.',date:'2023-09-21 14:27:10'},
-//   {source:'Kiyv',text:'It is 22 degrees in Kiyv today.',date:'2023-09-21 15:37:10'},
-//   {source:'Kiyv',text:'It is 22 degrees in Kiyv today.',date:'2023-09-21 16:67:10'},
-//   {source:'Kharkiv',text:'It is 24 degrees in Kharkiv today.',date:'2023-09-21 14:11:21'},
-//   {source:'Kharkiv',text:'It is 24 degrees in Kharkiv today.',date:'2023-09-21 15:26:31'},
-//   {source:'Kharkiv',text:'It is 24 degrees in Kharkiv today.',date:'2023-09-21 17:40:24'},
-//   {source:'Kharkiv',text:'It is 24 degrees in Kharkiv today.',date:'2023-09-21 19:53:51'},
-//   {source:'Dnipro',text:'It is 25 degrees in Dnipro today.',date:'2023-09-21 15:24:48'},
-//   {source:'Lviv',text:'It is 20 degrees in Lviv today.',date:'2023-09-21 16:00:09'},
-//   {source:'Lviv',text:'It is 20 degrees in Lviv today.',date:'2023-09-21 17:00:19'},
-// ];
-
-// function weatherNotification(currentWeatherNotification) {
-//   const result = {};
-
-//   currentWeatherNotification.forEach(({source, text, date}) => {
-//     if (!result[source]) {
-//       result[source] = [{text, date}];
-//     } else {
-//       result[source].push({text, date});
-//     }
-//   });
-
-//   return result;
-// }
-// console.log(weatherNotification(currentWeatherNotification));
-
-
 
