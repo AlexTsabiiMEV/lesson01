ee7cc8e3f4d12b852664485d567045cb200d3bab
Alex Tsabii <lsashokel@gmail.com>
2023-09-27 15:42:04 +0300

    Rework home work 11

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
 // 1) Вам необхідно написати функцію-декоратор logArguments(fn), яка приймає на вхід функцію і додає можливість логувати всі аргументи, передані у функцію-аргумент.
 
-
 function logArguments(fn) {
-  return fn.bind(null, ...arguments);
+  return function (...args) {
+    console.log(`Arguments: ${args}`);
+    return fn.bind(this)(...args);
+  }
 }
 
-const fullName = {
-  firstName: 'Alex',
-  surename: 'Tsabii',
-};
+const newArgs = logArguments(function newArgs(value1, value2) {
+  return value1 + value2;
+});
 
-function printFullName(firstName, surename) {
-  return (fullName.firstName + ' ' + fullName.surename);
-}
-
-const decoratedFunction = logArguments(printFullName);
-
-console.log(decoratedFunction(fullName.firstName, fullName.surename));
+console.log(newArgs(1, 2));
 
